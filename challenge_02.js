966588d8825f0dfdf6fd438b191757a91a2c265f
JGRO182 <j-p.grosse-kohlbrecher@web.de>
2021-11-22 08:41:01 +0100

    update
    Signed-off-by: JGRO182 <j-p.grosse-kohlbrecher@web.de>

--- challenge_02.js
+++ challenge_02.js
@@ -16,3 +16,57 @@ Alter 13-17 // Cola
 
 /* Getränke-Challenge */
 
+
+const name = prompt("Bitte geben Sie Ihren Namen ein");
+const ageString = prompt("Bitte geben Sie Ihr alter ein");
+const age = parseInt(ageString);
+
+/*******  mit Switch  *******/
+
+// switch (true) {
+//     case age >= 0 && age < 6:
+//         console.log(name +" trinkt Milch");
+//         break;
+//     case age >5 && age < 13:
+//         console.log(name +" trinkt Saft");
+//         break;
+//     case age >12 && age < 18:
+//         console.log(name +" trinkt Cola");
+//         break;
+//     case age >17:
+//         console.log(name +" trinkt Wein");
+//         break;
+//     default:
+//         console.log("Eingegebenes Alter nicht möglich");
+//         break;
+// }
+
+/*******  mit if / else if  *******/
+
+if (age >= 0 && age < 6){
+    console.log(name +" trinkt Milch");
+}else if (age >5 && age < 13){
+    console.log(name +" trinkt Saft");
+}else if(age >12 && age < 18){
+    console.log(name +" trinkt Cola");
+}else if(age >17){
+    console.log(name +" trinkt Wein");
+}else{
+    console.log("Eingegebenes Alter nicht möglich");
+}
+
+
+
+
+// /********Ternäre Schreibweise **********/
+
+// console.log(
+// (age >= 0 && age < 6) ? name +" trinkt Milch": 
+// (age >5 && age < 13) ? name +" trinkt Saft" : 
+// (age >12 && age < 18) ? name +" trinkt Cola" : 
+// (age >17) ? name +" trinkt Wein" : "Eingegebenes Alter nicht möglich"
+// )
+
+
+
+
